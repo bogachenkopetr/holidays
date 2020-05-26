943aaa18d2de87f625c92ffac8251f0022109dae
bogachenkopetr <56554801+bogachenkopetr@users.noreply.github.com>
2020-05-26 09:49:51 +0300

    добавил скрипт для properties-js

--- main.js
+++ main.js
@@ -7,7 +7,24 @@ $(document).ready(function(){
          $('.topnav').hide();
           $('.burger_menu').show();
      });
+    
+    
+    let $origImg; // содержит элемент большой картинки
+    let origSrc; // содержит путь к большой картинке
+    $('.properties_photo_slider img')
+        .mouseover(function(e){
+            let $img = $(e.target);
+            let imgSrc = $img.attr('src');
+            $origImg = $img.parent().siblings('.branding');
+            origSrc = $origImg.attr('src');
+            $origImg.attr('src', imgSrc);
+        })
+        .mouseleave(function(e){
+            $origImg.attr('src', origSrc);
+        });
 });
+
+
 
 
     
